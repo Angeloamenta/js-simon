75acb6507d0a01abd8033372f528e89b62f3dd74
Angeloamenta <angeloamenta7@gmail.com>
2021-12-03 16:43:50 +0100

    aggiunto timer

--- js/script.js
+++ js/script.js
@@ -10,7 +10,8 @@ const container = document.querySelector(".container");
 // creo array per conservare i numeri da usare per il confronto
 const numbers = [];
 const listNumbers = [];
-
+let daIndovinare = 0;
+let indovinati = 0;
 // inserisco 5 numeri casuali dentro container. uso un array per conservarli in memoria
 while (numbers.length < 5) {
     const number = Math.floor(Math.random() * 10) + 1; 
@@ -31,12 +32,45 @@ setTimeout(myFunction, 3000);
 function myFunction(){
     container.innerHTML = "";
     // chiedo al''utente di inserire 5 numeri e lo faccio con un prompt ed un for che cicla per 5 volte la domanda
-    for (let i = 0; i < 5; i++) {
-        let userNumbers = parseInt(prompt("inserisci 5 numeri"));
-        console.log(userNumbers);
-        listNumbers.push(userNumbers);
-    }
-    console.log(listNumbers);
+
+    // dopo aver tentato con JSON.stringify, faccio un ciclo for di 5 ( ovvero i numeri presenti nell'array numbers, si sarebbe potuto usare anche .length).
+    // girando sull'array ogni volta che:
+
+    // userNumbers ( quello inserito dall'utente) è == a numbers[i] ovvero un elemento presente nell'array numbers, il numero verrà pushato nell'array vuoto chiamato list numbers
+    
+
+    // messo un timer prima di permettere all'utente di dare il suo numero
+    let time = 10;
+
+    const timer = setInterval(() => {
+        if (time > 0) {
+            time -= 1;
+            console.log(time)
+        }else{
+            clearInterval(timer)
+        }
+
+        for (let i = 0; i < 5; i++) {
+            let userNumbers = parseInt(prompt("inserisci 5 numeri"));
+            console.log(userNumbers);
+            if (userNumbers == numbers[i]) {
+                listNumbers.push(userNumbers);
+                
+            }
+        }
+        console.log(listNumbers);
+        
+        // dopo l'operazione descritta sopra vado a sovrascrivere l'html ed inserisco grazie ai `backtick` il mio testo: con il numero degli elementi indovinati (listNumbers.length) ed i numeri indovinati (listNumbers)
+        
+        if (listNumbers.length > 0) {
+           container.innerHTML = `<h1>Hai indovinato ${listNumbers.length} numeri. <br>
+           I numeri che hai indovinati sono: ${listNumbers}</h1>`
+        } else {
+           container.innerHTML = `<h1>Hai indovinato ${listNumbers.length} numeri.</h1>`
+        }
+     }, 5000);
+ 
 }
-
-
+console.log(JSON.stringify(numbers));
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+// quindi.. devo cercare sul mio array iniziale e confrontare se sono presenti i numeri che il mio utente ha inserito
